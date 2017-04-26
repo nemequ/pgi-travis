64a278bf2ba126cf0d389277b3f6816335ccc2de
Evan Nemerson <evan@nemerson.com>
2017-04-26 12:03:11 -0700

    Pipe download directly to tar instead of using a temporary file

--- install-pgi.sh
+++ install-pgi.sh
@@ -12,7 +12,6 @@
 # See <https://creativecommons.org/publicdomain/zero/1.0/> for
 # details.
 
-DESTINATION="${HOME}/pgi"
 TEMPORARY_FILES="/tmp"
 
 export PGI_SILENT=true
@@ -67,24 +66,23 @@ done
 if [ ! -e "${TEMPORARY_FILES}" ]; then
     mkdir -p "${TEMPORARY_FILES}"
 fi
+cd "${TEMPORARY_FILES}"
 
-if [ ! -e "${TEMPORARY_FILES}"/pgi.tar.gz ]; then
-    wget -cO "${TEMPORARY_FILES}"/pgi.tar.gz \
-	 -U "pgi-travis (https://github.com/nemequ/pgi-travis; ${TRAVIS_REPO_SLUG})" \
-	 --header="X-Travis-Build-Number: ${TRAVIS_BUILD_NUMBER}" \
-	 --header="X-Travis-Event-Type: ${TRAVIS_EVENT_TYPE}" \
-	 --header="X-Travis-Job-Number: ${TRAVIS_JOB_NUMBER}" \
-	 --referer="http://www.pgroup.com/products/community.htm" \
-	 "https://www.pgroup.com/support/downloader.php?file=pgi-community-linux-x64" || exit 1
-fi
+PGI_URL="https://www.pgroup.com/support/downloader.php?file=pgi-community-linux-x64"
+
+curl --location \
+     --user-agent "pgi-travis (https://github.com/nemequ/pgi-travis; ${TRAVIS_REPO_SLUG})" \
+     --referer "http://www.pgroup.com/products/community.htm" \
+     --header "X-Travis-Build-Number: ${TRAVIS_BUILD_NUMBER}" \
+     --header "X-Travis-Event-Type: ${TRAVIS_EVENT_TYPE}" \
+     --header "X-Travis-Job-Number: ${TRAVIS_JOB_NUMBER}" \
+     "${PGI_URL}" | tar zxf -
 
 if [ x"${VERBOSE}" = "xtrue" ]; then
     VERBOSE_SHORT="-v"
     VERBOSE_V="v"
 fi
 
-cd "${TEMPORARY_FILES}"
-tar zxf${VERBOSE_V}k pgi.tar.gz
 cd "${TEMPORARY_FILES}"/install_components && ./install
 
 PGI_VERSION=$(basename "${PGI_INSTALL_DIR}"/linux86-64/*.*/)
@@ -97,8 +95,8 @@ fi
 for file in "${PGI_INSTALL_DIR}"/linux86-64/"${PGI_VERSION}"/bin/*; do
     dest="${INSTALL_BINDIR}/$(basename "${file}")"
     if [ -x "${file}" ]; then
-	echo "#!/bin/sh -x" > "${dest}"
-	echo "PGI=${PGI_INSTALL_DIR} PGI_INSTALL=\"\${PGI}\"/linux86-64/${PGI_VERSION} LM_LICENSE_FILE=\"\${PGI}\"/license.dat ${file} \$@" >> "${dest}"
+	echo "#!/bin/sh" > "${dest}"
+	echo "PGI=${PGI_INSTALL_DIR} PGI_INSTALL=\"\${PGI}\"/linux86-64/${PGI_VERSION} ${file} \$@" >> "${dest}"
 	chmod 0755 "${dest}"
     fi
 done
